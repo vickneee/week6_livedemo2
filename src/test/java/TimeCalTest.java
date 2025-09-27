3f177caf944a5bc2d9f8d277a3f0006c225f2ca2
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-09-28 02:36:08 +0300

    Fix Test file (static method)

--- src/test/java/TimeCalTest.java
+++ src/test/java/TimeCalTest.java
@@ -3,10 +3,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TimeCalTest {
-TimeCal timecal = new TimeCal();
+
     @Test
     public void calTime() {
-        assertEquals(1.0, timecal.calTime(100,100), 0.001);
+        assertEquals(1.0, TimeCal.calTime(100,100), 0.001);
 
     }
 }
